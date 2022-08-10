090586d46c9fb827ad3ee59a91be60787712a18a
Facuaten89 <91574579+Facuaten89@users.noreply.github.com>
2022-08-09 21:27:04 -0300

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,49 @@
+
+const contenedorDeProductos = [
+    {
+        id: '1',
+        nombre: 'Igloo waterdog aral 8 personas',
+        precio: 15600,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        img: 'https://www.lg.com/co/images/televisores/md07527575/gallery/D_03.jpg',
+        categoria: 'A',
+        stock: 60
+    },
+    {
+        id: '2',
+        nombre: 'Soy el producto 2',
+        precio: 20,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        img: 'https://picsum.photos/200/300?img=20',
+        categoria: 'B',
+        stock:50
+    },
+    {
+        id: '3',
+        nombre: 'Soy el producto 3',
+        precio: 30,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        img: 'https://picsum.photos/200/300?img=30',
+        categoria: 'A',
+        stock: 80
+    },
+    {
+        id: '4',
+        nombre: 'Soy el producto 4',
+        precio: 40,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        img: 'https://picsum.photos/200/300?img=40',
+        categoria: 'B',
+       stock:2
+    },
+    {
+        id: '5',
+        nombre: 'Soy el producto 5',
+        precio: 50,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        img: 'https://picsum.photos/200/300?img=50',
+        categoria: 'A',
+        stock:10
+    },
+];
+
